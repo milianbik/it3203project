d3ff85c237d1f0813315319f475ab848939b9d0a
milianbik <bikmilian@gmail.com>
2025-12-06 23:25:46 -0500

    editing table and menu

--- script.js
+++ script.js
@@ -1,2 +1,65 @@
+//document.getElementById("dropdown-btn").addEventListener(click);
+
+
+
+
+
+
+
+const questions = [
+        {
+            question: "Question 1?",
+            answers: [
+                { text: "Answer 1", correct: false},
+                { text: "Answer 2", correct: true},
+                { text: "Answer 3", correct: false},
+                { text: "Answer 4", correct: false},
+            ]
+        },
+        {
+            question: "Question 2?",
+            answers: [
+                { text: "Answer 1", correct: false},
+                { text: "Answer 2", correct: true},
+                { text: "Answer 3", correct: false},
+                { text: "Answer 4", correct: false},
+            ]
+        },
+        {
+            question: "Question 3?",
+            answers: [
+                { text: "Answer 1", correct: false},
+                { text: "Answer 2", correct: true},
+                { text: "Answer 3", correct: false},
+                { text: "Answer 4", correct: false},
+            ]
+        },
+        {
+            question: "Question 4?",
+            answers: [
+                { text: "Answer 1", correct: false},
+                { text: "Answer 2", correct: true},
+                { text: "Answer 3", correct: true},
+                { text: "Answer 4", correct: false},
+            ]
+        },
+        {
+            question: "Question 5?",
+            answers: [
+                { text: "Answer 1", correct: false},
+                { text: "Answer 2", correct: true},
+                { text: "Answer 3", correct: false},
+                { text: "Answer 4", correct: false},
+            ]
+        }
+        
+    ]
+    function name(){
+        blah
+    }
+
+    function name2(){
+        blah
+    }
 
 
